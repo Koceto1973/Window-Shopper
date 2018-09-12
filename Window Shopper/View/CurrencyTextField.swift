35ce0f8fac1f757a4db4d4403f23b6199c6f2a36
K.K <koceto1973@yahoo.ca>
2018-09-12 16:15:54 +0300

    Lesson 1
    bug fixed

--- Window Shopper/View/CurrencyTextField.swift
+++ Window Shopper/View/CurrencyTextField.swift
@@ -8,21 +8,34 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
 
+    // app IB time
+    override func prepareForInterfaceBuilder() {
+       customizeView()
+    }
+    
+    // app runtime
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2534246575)  // equivalent to uicolor.white
+        customizeView()
+      
+    }
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2534246575)  // equivalent to uicolor.white, with amended opacity value
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+                
         // providing value to the optional property
         if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [ .backgroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)] )
+            let place = NSAttributedString(string: p, attributes: [ .foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)] )
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
     }
 
 }
+
+
